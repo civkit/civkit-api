ef0b4f29d719d2a02eebdad6236ecf277efaacfc
MarketMadi <market_madi1@protonmail.com>
2024-07-11 20:45:34 +0530

    fixed invoice error

--- server.ts
+++ server.ts
@@ -10,31 +10,43 @@ import {
   handleFiatReceived,
   settleHoldInvoicesByOrderIdService,
 } from './services/invoiceService.js';
-import { registerUser, authenticateUser,   pollAndCompleteRegistration } from './services/userService.js';
+import { registerUser, authenticateUser, pollAndCompleteRegistration } from './services/userService.js';
 import orderRoutes from './routes/orderRoutes.js';
 import payoutsRoutes from './routes/payoutRoutes.js';
 import settleRoutes from './routes/settleRoutes.js';
 import { initializeNDK } from './config/ndkSetup.js';
 import { checkAndCreateChatroom, updateAcceptOfferUrl } from './services/chatService.js';
-import { query, pool } from './config/db.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import submitToMainstayRoutes from './routes/submitToMainstay.js';
+import { PrismaClient } from '@prisma/client';
+import { createOrder } from './controllers/orderController';
+import { createOrder } from './controllers/orderController.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const prisma = new PrismaClient();
+
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3001',
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization',
-}));
-
-
-
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000', 'http://localhost:5173'];
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    exposedHeaders: ['Content-Length', 'X-Custom-Header'],
+    credentials: true,
+  })
+);
 
 app.post('/api/register', async (req, res) => {
   const { username, password } = req.body;
@@ -47,12 +59,11 @@ app.post('/api/register', async (req, res) => {
         id: user.id,
         username: user.username,
         created_at: user.created_at,
-        invoice: user.invoice
+        invoice: user.invoice,
       },
-      invoice: user.invoice  // Display the invoice to regidster
+      invoice: user.invoice,
     });
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Registration failed', error: error.message });
   }
 });
@@ -66,19 +77,23 @@ setInterval(async () => {
   }
 }, 20000); // 20 seconds
 
-
 // User Login
 app.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+
   try {
-    const { username, password } = req.body;
+    console.log(`Login attempt for user: ${username}`);
     const user = await authenticateUser(username, password);
+    console.log(`User authenticated: ${user.username}`);
     const token = generateToken(user);
     res.json({ token });
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
+    console.error(`Login failed for user: ${username ? username : 'unknown user'} - Error: ${error.message}`);
     res.status(401).json({ message: 'Login failed', error: error.message });
   }
 });
+
+
 
 app.post('/api/holdinvoice', authenticateJWT, async (req, res) => {
   try {
@@ -86,7 +101,6 @@ app.post('/api/holdinvoice', authenticateJWT, async (req, res) => {
     const result = await postHoldinvoice(amount_msat, label, description);
     res.json(result);
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: error.message });
   }
 });
@@ -96,7 +110,6 @@ app.post('/api/holdinvoicelookup', authenticateJWT, async (req, res) => {
     const result = await holdInvoiceLookup(req.body.payment_hash);
     res.json(result);
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: error.message });
   }
 });
@@ -104,13 +117,14 @@ app.post('/api/holdinvoicelookup', authenticateJWT, async (req, res) => {
 app.use('/api/orders', authenticateJWT, orderRoutes);
 app.use('/api/payouts', authenticateJWT, payoutsRoutes);
 
+app.post('/api/order',)
+
 app.post('/api/sync-invoices', authenticateJWT, async (req, res) => {
   try {
     await syncInvoicesWithNode();
     res.status(200).json({ message: 'Invoices synchronized successfully' });
   } catch (error) {
     console.error('Failed to sync invoices:', error);
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Failed to synchronize invoices', error: error.message });
   }
 });
@@ -120,7 +134,6 @@ app.get('/api/sync-payouts', authenticateJWT, async (req, res) => {
     await syncPayoutsWithNode();
     res.status(200).json({ message: 'Payouts synchronized successfully' });
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Failed to synchronize payouts', error: error.message });
   }
 });
@@ -132,7 +145,6 @@ app.post('/api/fiat-received', authenticateJWT, async (req, res) => {
     res.status(200).json({ message: 'Fiat received processed successfully' });
   } catch (error) {
     console.error('Error processing fiat received:', error);
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Error processing fiat received', error: error.message });
   }
 });
@@ -143,21 +155,13 @@ app.post('/api/settle-holdinvoices-by-order', authenticateJWT, async (req, res) 
     const result = await settleHoldInvoicesByOrderIdService(orderId);
     res.status(200).json({ message: 'Hold invoices settled successfully', result });
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Error settling hold invoices', error: error.message });
   }
 });
 
-// Initialize NDK and create identity
-initializeNDK().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
 
 app.post('/api/check-accepted-invoices', authenticateJWT, async (req, res) => {
   try {
-    // @ts-expect-error TS(2304): Cannot find name 'checkAndUpdateAcceptedInvoices'.
     await checkAndUpdateAcceptedInvoices();
     res.status(200).send({ message: 'Invoices checked and updated successfully.' });
   } catch (error) {
@@ -167,36 +171,32 @@ app.post('/api/check-accepted-invoices', authenticateJWT, async (req, res) => {
 
 app.post('/api/check-and-create-chatroom', authenticateJWT, async (req, res) => {
   const { orderId } = req.body;
-  // @ts-expect-error TS(2339): Property 'user' does not exist on type 'Request<{}... Remove this comment to see the full error message
   const userId = req.user.id; // Assuming `req.user` contains the authenticated user's details
 
   try {
     // Fetch the order details
-    const orderResult = await query('SELECT * FROM orders WHERE order_id = $1', [orderId]);
-    if (orderResult.rows.length === 0) {
+    const order = await prisma.orders.findUnique({
+      where: { order_id: parseInt(orderId) },
+    });
+    if (!order) {
       return res.status(404).json({ message: 'Order not found' });
     }
 
-    const order = orderResult.rows[0];
     console.log('Order details:', order);
     console.log('User ID:', userId);
 
     // Check if the user is the maker or taker of the order
-    // @ts-expect-error TS(2339): Property 'customer_id' does not exist on type 'any... Remove this comment to see the full error message
     if (order.customer_id !== userId && order.taker_customer_id !== userId) {
       return res.status(403).json({ message: 'You are not authorized to access this chatroom' });
     }
 
     // Proceed to create or check chatroom
-    // @ts-expect-error TS(2339): Property 'makeOfferUrl' does not exist on type '{ ... Remove this comment to see the full error message
     const { makeOfferUrl, acceptOfferUrl } = await checkAndCreateChatroom(orderId);
     res.status(200).json({ makeChatUrl: makeOfferUrl, acceptChatUrl: acceptOfferUrl });
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Failed to create chatroom', error: error.message });
   }
 });
-
 
 // Endpoint to update accept-offer URL
 app.post('/api/update-accept-offer-url', authenticateJWT, async (req, res) => {
@@ -205,7 +205,6 @@ app.post('/api/update-accept-offer-url', authenticateJWT, async (req, res) => {
     await updateAcceptOfferUrl(chat_id, accept_offer_url);
     res.status(200).json({ message: 'Accept-offer URL updated successfully' });
   } catch (error) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ message: 'Failed to update accept-offer URL', error: error.message });
   }
 });
@@ -215,26 +214,25 @@ app.use('/api/settle', settleRoutes);
 // Get all orders
 app.get('/api/orders', authenticateJWT, async (req, res) => {
   try {
-    // @ts-expect-error TS(2554): Expected 2 arguments, but got 1.
-    const result = await query('SELECT * FROM orders');
-    res.status(200).json(result.rows);
+    const result = await prisma.orders.findMany();
+    res.status(200).json(result);
   } catch (err) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: err.message });
   }
 });
 
-//Get order by ID
+// Get order by ID
 app.get('/api/orders/:orderId', authenticateJWT, async (req, res) => {
   const { orderId } = req.params;
   try {
-    const result = await query('SELECT * FROM orders WHERE order_id = $1', [orderId]);
-    if (result.rows.length === 0) {
+    const result = await prisma.orders.findUnique({
+      where: { order_id: parseInt(orderId) },
+    });
+    if (!result) {
       return res.status(404).json({ error: 'Order not found' });
     }
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result);
   } catch (err) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: err.message });
   }
 });
@@ -242,14 +240,30 @@ app.get('/api/orders/:orderId', authenticateJWT, async (req, res) => {
 // Get invoice by order ID
 app.get('/api/invoice/:orderId', authenticateJWT, async (req, res) => {
   const { orderId } = req.params;
+  console.log(`Fetching invoices for order ID: ${orderId}`);
+
   try {
-    const result = await query('SELECT * FROM invoices WHERE order_id = $1', [orderId]);
-    if (result.rows.length === 0) {
+    const result = await prisma.invoices.findMany({
+      where: { order_id: parseInt(orderId) },
+    });
+
+    console.log('Invoices Response:', result);
+
+    if (result.length === 0) {
+      console.log('No invoices found for the given order ID.');
       return res.status(404).json({ error: 'Invoice not found' });
     }
-    res.status(200).json(result.rows[0]);
+
+    // Serialize the BigInt value
+    const invoice = {
+      ...result[0],
+      amount_msat: result[0].amount_msat.toString(),
+    };
+
+    console.log('Returning the first invoice in the result set.');
+    res.status(200).json(invoice);
   } catch (err) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
+    console.error('Error fetching invoices:', err);
     res.status(500).json({ error: err.message });
   }
 });
@@ -257,13 +271,17 @@ app.get('/api/invoice/:orderId', authenticateJWT, async (req, res) => {
 app.get('/api/taker-invoice/:orderId', authenticateJWT, async (req, res) => {
   const { orderId } = req.params;
   try {
-    const result = await query('SELECT * FROM invoices WHERE order_id = $1 AND user_type = $2', [orderId, 'taker']);
-    if (result.rows.length === 0) {
+    const result = await prisma.invoices.findMany({
+      where: {
+        order_id: parseInt(orderId),
+        user_type: 'taker'
+      },
+    });
+    if (result.length === 0) {
       return res.status(404).json({ error: 'Taker invoice not found' });
     }
-    res.status(200).json(result.rows);
+    res.status(200).json(result);
   } catch (err) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: err.message });
   }
 });
@@ -271,33 +289,39 @@ app.get('/api/taker-invoice/:orderId', authenticateJWT, async (req, res) => {
 app.get('/api/full-invoice/:orderId', authenticateJWT, async (req, res) => {
   const { orderId } = req.params;
   try {
-    const result = await query('SELECT * FROM invoices WHERE order_id = $1 AND invoice_type = $2', [parseInt(orderId, 10), 'full']);
-    if (result.rows.length === 0) {
+    const result = await prisma.invoices.findFirst({
+      where: {
+        order_id: parseInt(orderId),
+        invoice_type: 'full',
+      },
+    });
+    if (!result) {
       return res.status(404).json({ error: 'Full invoice not found' });
     }
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result);
   } catch (err) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: err.message });
   }
 });
 
-// endpoint to lookup full invoice by payment hash
+// Endpoint to lookup full invoice by payment hash
 app.post('/api/fullinvoicelookup', authenticateJWT, async (req, res) => {
   const { payment_hash } = req.body;
   try {
-    const result = await query('SELECT * FROM invoices WHERE payment_hash = $1 AND invoice_type = $2', [payment_hash, 'full']);
-    if (result.rows.length === 0) {
+    const result = await prisma.invoices.findFirst({
+      where: {
+        payment_hash: payment_hash,
+        invoice_type: 'full',
+      },
+    });
+    if (!result) {
       return res.status(404).json({ error: 'Full invoice not found' });
     }
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result);
   } catch (err) {
-    // @ts-expect-error TS(2571): Object is of type 'unknown'.
     res.status(500).json({ error: err.message });
   }
 });
-
-
 
 // Call this function periodically
 setInterval(async () => {
@@ -316,15 +340,16 @@ app.post('/api/get-invoice', async (req, res) => {
   }
 
   try {
-    const query = 'SELECT invoice, payment_hash, status FROM users WHERE username = $1';
-    const values = [username];
-    const { rows } = await pool.query(query, values);
+    const user = await prisma.user.findUnique({
+      where: { username: username },
+      select: { invoice: true, payment_hash: true, status: true },
+    });
 
-    if (rows.length === 0) {
+    if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    const { invoice, payment_hash, status } = rows[0];
+    const { invoice, payment_hash, status } = user;
     res.status(200).json({ invoice, payment_hash, status });
   } catch (error) {
     console.error('Error fetching invoice:', error);
@@ -333,3 +358,23 @@ app.post('/api/get-invoice', async (req, res) => {
 });
 
 app.use('/api', submitToMainstayRoutes);
+
+let server;
+
+initializeNDK().then(() => {
+  if (!server) {
+    server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+    server.on('error', (error) => {
+      if (error.code === 'EADDRINUSE') {
+        console.error(`Port ${PORT} is already in use. Try a different port.`);
+      } else {
+        console.error('An error occurred while starting the server:', error);
+      }
+    });
+  } else {
+    console.log('Server is already running.');
+  }
+});
