9714b70fdab197461cc3dac624e77cd2a0ff2b90
MarketMadi <market_madi1@protonmail.com>
2024-09-18 16:52:42 +0530

    Remove dist folder from version control

--- dist/routes/payoutRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { submitPayout } from '../controllers/payoutController.js';
-const router = express.Router();
-// POST request to submit payout
-router.post('/submit', submitPayout);
-export default router;
